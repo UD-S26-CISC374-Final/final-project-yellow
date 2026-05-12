5ea7713c18d769cae34be7d63d38ebcccdb69fc7
Matthew Goldstein <mattg0811@yahoo.com>
2026-05-12 11:35:49 -0400

    Removed top panel that had the change scene button

--- src/App.tsx
+++ src/App.tsx
@@ -102,31 +102,6 @@ function App() {
                 ref={phaserRef}
                 onCurrentActiveSceneChange={onCurrentSceneChange}
             />
-            <div id="ui-panel">
-                <div>
-                    <button className="button" onClick={changeScene}>
-                        Change Scene
-                    </button>
-                </div>
-                <div>
-                    <button
-                        disabled={canMoveSprite}
-                        className="button"
-                        onClick={moveSprite}
-                    >
-                        Toggle Movement
-                    </button>
-                </div>
-                <div className="spritePosition">
-                    Sprite Position:
-                    <pre>{`{\n  x: ${spritePosition.x}\n  y: ${spritePosition.y}\n}`}</pre>
-                </div>
-                <div>
-                    <button className="button" onClick={addSprite}>
-                        Add New Sprite
-                    </button>
-                </div>
-            </div>
         </div>
     );
 }
